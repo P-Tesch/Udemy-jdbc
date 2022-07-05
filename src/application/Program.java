b05cbf8251223eec5cfc1177da12f0a951b7a7d2
pedro tesch <pedrohenriquetesch@gmail.com>
2022-07-05 00:44:05 -0300

    Added class Department

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department department = new Department("Electronics", 1);
+		System.out.println(department);
+
+	}
+
+}
