79873228247819d5f895ce6b61b0ffa3705382a2
pedro tesch <pedrohenriquetesch@gmail.com>
2022-07-22 01:47:48 -0300

    Added NewDepartmentFormView

--- src/gui/DepartmentViewController.java
+++ src/gui/DepartmentViewController.java
@@ -1,17 +1,26 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.Program;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -37,8 +46,8 @@ public class DepartmentViewController implements Initializable {
 	private TableColumn<Department, String> tableColumnName;
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("a");
+	public void onBtNewAction(ActionEvent event) {
+		this.createNewDepartmentFormView("/gui/NewDepartmentFormView.fxml", Utils.currentStage(event));
 	}
 	
 	public void setDepartmentService(DepartmentService departmentService) {
@@ -58,5 +67,23 @@ public class DepartmentViewController implements Initializable {
 		}
 		this.departmentList = FXCollections.observableArrayList(departmentService.findAll());
 		this.tableViewDepartment.setItems(departmentList);
+	}
+	
+	private void createNewDepartmentFormView(String viewPath, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(viewPath));
+			Pane pane = loader.load();
+			
+			Stage formStage = new Stage();
+			formStage.setTitle("Enter department data");
+			formStage.setScene(new Scene(pane));
+			formStage.setResizable(false);
+			formStage.initOwner(parentStage);
+			formStage.initModality(Modality.WINDOW_MODAL);
+			formStage.showAndWait();
+		}
+		catch (IOException e) {
+			Alerts.showAlert("Error", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
